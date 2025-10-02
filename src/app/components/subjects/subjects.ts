2ef5a304fe2056144b8d7e468b400f9836855599
shovon-gt <shovon.biswas@gigatechltd.com>
2025-10-03 00:32:29 +0600

    Fixed subject data UI issue

--- src/app/components/subjects/subjects.ts
+++ src/app/components/subjects/subjects.ts
@@ -3,40 +3,30 @@ import { Component, inject } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatList, MatListItem } from '@angular/material/list';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { switchMap } from 'rxjs';
-import { Services } from '../Services/services';
+import { map, Observable, switchMap } from 'rxjs';
+import { Services, Student } from '../Services/services';
 
 @Component({
   selector: 'app-subjects',
   imports: [RouterLink, MatIcon, MatList, MatListItem, CommonModule],
   templateUrl: './subjects.html',
-  styleUrl: './subjects.scss'
+  styleUrls: ['./subjects.scss']
 })
 export class Subjects {
-  studentsData: any;
-  topper: any = {
-    studentName: '',
-    marks: Number.MIN_SAFE_INTEGER
-  }
-  route = inject(ActivatedRoute);
-  studensService = inject(Services);
+  private route = inject(ActivatedRoute);
+  private studentService = inject(Services);
 
-  constructor(){
-    this.route.params.pipe(switchMap((res: any) => this.studensService.getSubjectData(res.subjectName)
-    )).subscribe((res: any) => {
-      this.studentsData = res;
-      this.updateTopper();
-    });
-  }
+  // Observable for students data
+  studentsData$: Observable<Student[]> = this.route.params.pipe(
+    switchMap(params => this.studentService.getSubjectData(params['subjectName']))
+  );
 
-  updateTopper() {
-    for (let i = 0; i < this.studentsData.length; i++) {
-      if (this.topper.marks < this.studentsData[i].marks) {
-        debugger
-        console.log(this.studentsData[i]);
-        this.topper = this.studentsData[i];
-      }
-    }
-  }
+  // Observable for topper
+  topper$: Observable<Student | null> = this.studentsData$.pipe(
+    map(students => {
+      if (!students || students.length === 0) return null;
+      return students.reduce((top, s) => s.marks > top.marks ? s : top, students[0]);
+    })
+  );
 
 }
